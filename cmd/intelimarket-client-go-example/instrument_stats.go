62fa6792e72333660ee0760d6830c0ae4f68ca0e
Rodrigo Strauss <rodrigo.strauss@intelitrader.com.br>
2020-04-11 10:49:03 -0300

    MARKET-321: implementa InteliMarketConnection.SubscribeGroupProperties()

---
+++ cmd/intelimarket-client-go-example/instrument_stats.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"sort"
+
+	"bitbucket.org/intelitrader/intelimarket-client-go/pkg/intelimarketclient"
+)
+
+type instrumentStats struct {
+	symbol     string
+	eventCount int
+}
+
+type instrumentsEventsStatsPrinter struct {
+	instrumentsEventsStats      map[string]*instrumentStats
+	instrumentStatsLogStep      int
+	instrumentStatsEventCount   int
+	instrumentCountOnLogMessage int
+}
+
+func NewInstrumentsEventsStatsPrinter(instrumentCountOnLogMessage int) *instrumentsEventsStatsPrinter {
+	return &instrumentsEventsStatsPrinter{make(map[string]*instrumentStats), 10000, 0, instrumentCountOnLogMessage}
+}
+
+func (self instrumentStats) String() string {
+	return fmt.Sprintf("%v=%v", self.symbol, self.eventCount)
+}
+
+func (self instrumentsEventsStatsPrinter) LaunchAsyncStatisticsPrinter(channel <-chan intelimarketclient.PropertyChangeInfo) {
+	go self.asyncStatisticsPrinter(channel)
+}
+
+func (self instrumentsEventsStatsPrinter) asyncStatisticsPrinter(channel <-chan intelimarketclient.PropertyChangeInfo) {
+	for {
+		info := <-channel
+		if _, ok := self.instrumentsEventsStats[info.Symbol]; !ok {
+			self.instrumentsEventsStats[info.Symbol] = &instrumentStats{info.Symbol, 0}
+		}
+
+		self.instrumentsEventsStats[info.Symbol].eventCount++
+		self.instrumentStatsEventCount++
+
+		if self.instrumentStatsEventCount%self.instrumentStatsLogStep == 0 {
+			// saudades do LINQ...
+			var biggest []*instrumentStats
+			for _, v := range self.instrumentsEventsStats {
+				biggest = append(biggest, v)
+			}
+			sort.Slice(biggest, func(i, j int) bool {
+				return biggest[i].eventCount > biggest[j].eventCount
+			})
+
+			log.Printf("STATS: eventCount=%v, perInstrument=%v", self.instrumentStatsEventCount, biggest[:self.instrumentCountOnLogMessage])
+		}
+	}
+}
--- internal/p9mdi_client_test.go
+++ internal/p9mdi_client_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestP9Connection(*testing.T) {
 
-	var connection = P9mdi_connect("demo.intelitrader.com.br", 2605)
+	var connection = P9mdi_connect("demo.intelitrader.com.br", 2605, 0)
 
 	P9mdi_disconnect(connection)
 }
